af5882ab2ee15973faf89b02030697c6b7ed38a6
Elmer960-ui <yagg0715@gmail.com>
2025-03-05 13:21:23 -0600

    hola

---
+++ FormMenu.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace win.rentas
+{
+    public partial class FormMenu : Form
+    {
+        public FormMenu()
+        {
+            InitializeComponent();
+        }
+
+        private void FormMenu_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void loginToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            var FormLogin = new FormLogin();
+            FormLogin.ShowDialog();
+        }
+
+        private void productosToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            var formProductos = new formproductos();
+            formproductos.MdiParent = this;
+            formproductos.show();
+        }
+    }
+}
