ac824cb008c2a1342f5f2fb69d23c819b2900d27
Elmer960-ui <yagg0715@gmail.com>
2025-03-06 08:51:47 -0600

    hasta el video

--- FormMenu.cs
+++ FormMenu.cs
@@ -19,20 +19,40 @@ namespace win.rentas
 
         private void FormMenu_Load(object sender, EventArgs e)
         {
+            Login();
+        }
 
+        private void Login();
+        {
+
+        var formLogin = new FormLogin();
+        FormLogin.showDialog();
         }
 
         private void loginToolStripMenuItem_Click(object sender, EventArgs e)
         {
-            var FormLogin = new FormLogin();
-            FormLogin.ShowDialog();
+            login();
         }
 
         private void productosToolStripMenuItem_Click(object sender, EventArgs e)
         {
             var formProductos = new formproductos();
-            formproductos.MdiParent = this;
+            formproductos. MdiParent = this;
             formproductos.show();
+        }
+
+        private void clientesToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            var formClientes = new formClientes();
+            formClientes. MdiParent = this;
+                formClientes.show();
+        }
+
+        private void rentarToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            var formRentas = new FormRentas();
+            FormRentas. MdiParent = this;
+            FormRentas.Show();
         }
     }
 }
